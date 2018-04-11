69bc417c11a0bcc5e9a35917a7b42ba21e523f23
JASPINDER SING <jaspindersingh83@gmail.com>
2018-04-11 16:01:56 -0700

    Basic Bookmarks App

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+
+class Bookmark(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=200, unique=True)
+    notes = models.TextField('Notes', blank=True)
+    url = models.URLField('URL', unique=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
