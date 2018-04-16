d4061cadff086f30d65f45e83c464c3f61ab1956
JASPINDER SING <jaspindersingh83@gmail.com>
2018-04-16 09:49:37 -0700

    Added Boilerplate for Notes App

---
+++ Notes/models.py
@@ -0,0 +1,12 @@
+from uuid import uuid4
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+  id = models.UUIDField(primary_key = True, default=uuid4, editable=False) 
+  title = models.CharField(max_length = 200)
+  content = models.TextField(blank = True)
+  createdAt = models.DateTimeField(auto_now_add = True)
+  lastModified = models.DateTimeField(auto_now = True)
+  user = models.ForeignKey (User, on_delete = models.CASCADE)
--- djorg/settings.py
+++ djorg/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     # our apps
     'bookmarks',
+    'Notes',
 ]
 
 MIDDLEWARE = [
